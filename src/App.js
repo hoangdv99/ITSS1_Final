65b98908dbda56bf7591b6a9a430eebfbccb3380
hoangdv99 <hoangdv257@gmail.com>
2021-09-08 11:06:44 +0700

    final

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import useInput from './useInput';
+import './App.css';
 
 function App() {
-  const {value, data, onChange, confirm} = useInput('');
+  const { value, data, onChange, confirm } = useInput('');
 
   return (
-    <div>
-        <div>学生一覧:
-          { data }
-        </div>
-        <div>追加する名前を入力してください：</div>
-        <input type="text" value={ value } onChange={ onChange } />
-        <button onClick={ confirm }>確認</button>
-        <div>追加する名前：{ value }</div>
-        <div>新しい一覧：{ data }</div>
-    </div>  
+    <div className="app">
+      <div>学生一覧: [{ data.toString() }]</div>
+      <div>追加する名前を入力してください：</div>
+      <input type="text" value={ value } onChange={ onChange } />
+      <button onClick={ confirm }>確認</button>
+      <div>追加する名前：{ value }</div>
+      <div>新しい一覧：[{ data.toString() }]</div>
+    </div>
   );
 }
 
